d704ccdc0f3cd7354068624a500a08f85b6e9a02
Adrian Medina <62039726+kadrian2102@users.noreply.github.com>
2026-03-15 21:59:51 -0600

    added auth library

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework import status
 from .models import MenuItem
 from .serializers import MenuItemSerializer
 
@@ -11,4 +11,10 @@ def menu_items(request):
         items = MenuItem.objects.select_related('category').all()
         serialized_items = MenuItemSerializer(items, many=True)
         return Response(serialized_items.data)
+    
+    # elif request.method == 'POST':
+    #     serialized_items = MenuItemSerializer(data=request.data)
+    #     serialized_items.is_valid(raise_exception=True)
+    #     serialized_items.save()
+    #     return Response(serialized_items.data, status=status.HTTP_201_CREATED)
 
